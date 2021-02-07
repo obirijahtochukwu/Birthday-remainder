ce0c308decb1ae4321831fd87e218aef756e4b79
Obirijah Tochukwu <tobirijah@gmail.com>
2021-02-07 15:00:13 +0000

    first commit

---
+++ setup/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    image:
+      'img/product-.png',
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    image:
+      'img/product-1.png',
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    image:
+      'img/product-3.png',
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    image:
+      'img/product-4.png',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'img/product-5.png',
+  },
+];
